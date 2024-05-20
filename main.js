ad9720a6610e31a8d0c47f316c26825bae87775a
xueliang.wu <xueliang.wu@aispeech.com>
2024-05-20 11:42:36 +0800

    pages

--- main.js
+++ main.js
@@ -5,18 +5,21 @@ let mainWindow
 // 创建主窗口，设置了宽高等信息
 function createWindow() {
     mainWindow = new BrowserWindow({
-        fullscreen: true,
         webPreferences: {
             // node集成，即是否注入node能力
             nodeIntegration: true
         }
     })
 
-    // 加载主页面内容 index.html
-    // 改为使用loadURL加载 draw.io 的url地址
-    mainWindow.maximize();
-    mainWindow.loadURL('https://www.baidu.com/')
+    var webUrl = process.env.ELECTRON_WEB_URL
+    if (!webUrl) {
+        webUrl = 'http://localhost:8888'
+    }
 
+    // 加载主页面内容 index.html
+    mainWindow.maximize();
+    mainWindow.setMenu(null);
+    mainWindow.loadURL(webUrl)
     mainWindow.on('closed', function () {
         mainWindow = null
     })
